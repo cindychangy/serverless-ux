2027007d246085d19a9ce5a1e37295cf8b088dfa
Cindy Chang <cindyisachang@gmail.com>
2023-02-16 09:57:31 +0100

    mods to cloud setup

---
+++ src/layouts/cloud/homepage.js
@@ -0,0 +1,235 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiPageSection,
+  EuiPanel,
+  EuiTitle,
+  EuiButton,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  EuiHealth,
+  EuiSpacer,
+  EuiLink,
+  EuiText,
+  EuiTextColor,
+  EuiSearchBar,
+  EuiHorizontalRule,
+} from '@elastic/eui';
+import Navbar from '../../components/navbar';
+
+const iconStyle = css`
+  background: #f5f7fa;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 3px 2px rgb(0 0 0 / 10%);
+`;
+
+const CloudHomepageLayout = ({ children }) => {
+  const iconTitle = (icon, title) => {
+    return (
+      <EuiFlexGroup alignItems="center" gutterSize="s">
+        <EuiFlexItem grow={false}>
+          <div css={iconStyle}>
+            <EuiIcon type={icon} size="m" />
+          </div>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiTitle size="xs">
+            <h4>{title}</h4>
+          </EuiTitle>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    );
+  };
+
+  return (
+    <>
+      <Navbar
+        isCloud
+        breadcrumbs={[
+          {
+            text: 'Cloud',
+          },
+        ]}
+      />
+      <EuiPage>
+        <EuiPageBody
+          paddingSize="none"
+          css={css`
+            margin: auto;
+            max-width: 1600px;
+          `}>
+          <EuiPageSection>
+            <EuiFlexGroup gutterSize="l" alignItems="flexStart">
+              <EuiFlexItem grow={2}>
+                <EuiFlexGroup alignItems="center">
+                  <EuiFlexItem>
+                    <EuiTitle size="s">
+                      <h3>Elastic Cloud</h3>
+                    </EuiTitle>
+                  </EuiFlexItem>
+                  <EuiFlexItem grow={false}>
+                    <EuiButton iconType="plusInCircle" fill>
+                      Create
+                    </EuiButton>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+                {children}
+                <EuiSpacer size="l" />
+                <EuiFlexGroup>
+                  <EuiFlexItem>
+                    <EuiPanel grow={false} paddingSize="l">
+                      {iconTitle('documents', 'Documentation')}
+                      <EuiSpacer size="m" />
+                      <EuiSearchBar box={{ placeholder: 'Help me find...' }} />
+                      <EuiSpacer size="m" />
+                      <EuiLink href="">
+                        Elasticsearch Service on Elastic Cloud documentation
+                      </EuiLink>
+                      <EuiSpacer size="m" />
+                      <EuiLink href="">Elasticsearch documentation</EuiLink>
+                      <EuiSpacer size="m" />
+                      <EuiLink href="">Elasticsearch REST API</EuiLink>
+                    </EuiPanel>
+                    <EuiSpacer size="l" />
+                    <EuiPanel grow={false} paddingSize="l">
+                      {iconTitle('help', 'Support')}
+                      <EuiSpacer size="m" />
+                      <EuiText size="s">
+                        Having some trouble? Reach out to us.
+                      </EuiText>
+                      <EuiSpacer size="m" />
+                      <EuiButton>Contact support</EuiButton>
+                    </EuiPanel>
+                  </EuiFlexItem>
+                  <EuiFlexItem>
+                    <EuiPanel grow={false} paddingSize="l">
+                      {iconTitle('users', 'Community')}
+                      <EuiSpacer size="m" />
+                      <EuiLink href="">Join an ElasticON event</EuiLink>
+                      <EuiSpacer size="s" />
+                      <EuiText size="s">
+                        Hear success stories, lessons learned, tips, tricks,
+                        best practices, and funny anecdotes…
+                      </EuiText>
+                      <EuiSpacer size="l" />
+                      <EuiLink href="">
+                        Elastic Blasticians presents: A Fireside Chat with Dr.
+                        William Scott, IBM Master
+                      </EuiLink>
+                      <EuiSpacer size="s" />
+                      <EuiText size="xs">FEBRUARY 16, 18:00</EuiText>
+                      <EuiSpacer size="l" />
+                      <EuiLink href="">
+                        Improving visibility into modern telco networks: A
+                        consolidated look on 3G/4G to 5G, NWDAF and Radio
+                        Analytics
+                      </EuiLink>
+                      <EuiSpacer size="s" />
+                      <EuiText size="xs">FEBRUARY 21, 9:00</EuiText>
+                      <EuiSpacer size="m" />
+                      <EuiButton href="">Events portal</EuiButton>
+                      <EuiHorizontalRule />
+                      <EuiText size="s">
+                        Engage with our community!
+                        <a href="">Visit our forum</a>,
+                        <a href="">join us on Slack</a>, or
+                        <a href="">contribute to the Elastic Stack on GitHub</a>
+                        .
+                      </EuiText>
+                    </EuiPanel>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFlexItem>
+              <EuiFlexItem grow={1}>
+                <EuiFlexGroup direction="column">
+                  <EuiFlexItem>
+                    <EuiPanel grow={false} paddingSize="l">
+                      {iconTitle('cloudSunny', 'Cloud status')}
+                      <EuiSpacer size="m" />
+                      <EuiHealth color="success">
+                        All systems operational
+                      </EuiHealth>
+                    </EuiPanel>
+                  </EuiFlexItem>
+                  <EuiFlexItem grow={false}>
+                    <EuiPanel paddingSize="l">
+                      {iconTitle('documentEdit', 'News')}
+                      <EuiSpacer size="m" />
+                      <EuiLink href="">
+                        <EuiText
+                          size="s"
+                          css={css`
+                            font-weight: 500;
+                          `}>
+                          How to build comprehensive customer financial profiles
+                          with Elastic Cloud and Google Cloud
+                        </EuiText>
+                      </EuiLink>
+                      <EuiSpacer size="s" />
+                      <small>FEBRUARY 14, 2023</small> &nbsp; &nbsp;
+                      <EuiTextColor color="accent">New!</EuiTextColor>
+                      <EuiSpacer size="m" />
+                      <EuiLink href="">
+                        <EuiText
+                          size="s"
+                          css={css`
+                            font-weight: 500;
+                          `}>
+                          How tough was your workout? Take a closer look at
+                          Strava data through Kibana Lens
+                        </EuiText>
+                      </EuiLink>
+                      <EuiSpacer size="s" />
+                      <small>FEBRUARY 13, 2023</small> &nbsp; &nbsp;
+                      <EuiTextColor color="accent">New</EuiTextColor>
+                      <EuiSpacer size="m" />
+                      <EuiLink href="">
+                        <EuiText
+                          size="s"
+                          css={css`
+                            font-weight: 500;
+                          `}>
+                          Optimizing Strava data collection with Elastic APM and
+                          a custom script solution
+                        </EuiText>
+                      </EuiLink>
+                      <EuiSpacer size="s" />
+                      <small>FEBRUARY 6, 2023</small> &nbsp; &nbsp;
+                      <EuiTextColor color="accent">New</EuiTextColor>
+                    </EuiPanel>
+                  </EuiFlexItem>
+                  <EuiFlexItem>
+                    <EuiPanel grow={false} paddingSize="l">
+                      {iconTitle('training', 'Training')}
+                      <EuiSpacer size="m" />
+                      <EuiText>
+                        <h5>Go deeper with hands-on training</h5>
+                      </EuiText>
+                      <EuiSpacer size="m" />
+                      <EuiText size="s">
+                        Build essential skills and learn Elastic with free
+                        introductory training in the Elastic Learning Portal
+                      </EuiText>
+                      <EuiSpacer size="m" />
+                      <EuiButton>Elastic Learning Portal</EuiButton>
+                    </EuiPanel>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiPageSection>
+        </EuiPageBody>
+      </EuiPage>
+    </>
+  );
+};
+
+export default CloudHomepageLayout;
---
+++ src/pages/ic-2/cloud.js
@@ -0,0 +1,196 @@
+/** @jsxImportSource @emotion/react */
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { css } from '@emotion/react';
+import {
+  EuiPanel,
+  EuiText,
+  EuiLink,
+  EuiSpacer,
+  EuiBadge,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  EuiTable,
+  EuiTableHeader,
+  EuiTableHeaderCell,
+  EuiTableRow,
+  EuiTableRowCell,
+  EuiTableBody,
+  useEuiTheme,
+} from '@elastic/eui';
+import CloudHomepageLayout from '../../layouts/cloud/homepage';
+
+const CloudHomepage = () => {
+  const { euiTheme } = useEuiTheme();
+  const router = useRouter();
+
+  const logoCircle = css`
+    border-radius: 50%;
+    height: 32px;
+    width: 32px;
+    box-shadow: 0 3px 2px rgb(0 0 0 / 10%);
+    background: ${euiTheme.colors.lightestShade};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  const tableStyles = css`
+    .euiTableHeaderCell:nth-child(4),
+    .euiTableRowCell:nth-child(4) {
+      .euiTableCellContent {
+        justify-content: flex-end;
+      }
+    }
+
+    .euiTableHeaderCell:nth-child(5),
+    .euiTableRowCell:nth-child(5) {
+      .euiTableCellContent {
+        justify-content: flex-end;
+      }
+    }
+  `;
+
+  const TABLE_HEADER_DEPLOYMENTS = [
+    'Deployment',
+    'Status',
+    'Version',
+    'Cloud provider & region',
+    'Actions',
+  ];
+
+  const TABLE_ITEMS_DEPLOYMENTS = [
+    {
+      name: 'Security xyz2064',
+      status: <EuiBadge color="success">Healthy</EuiBadge>,
+      version: '8.5.0',
+      region: 'GCP - Iowa (us-central1)',
+    },
+  ];
+
+  const TABLE_HEADER_PROJECTS = [
+    'Project',
+    'Type',
+    'Cloud provider & region',
+    'Actions',
+  ];
+
+  const TABLE_ITEMS_PROJECTS = [
+    {
+      name: 'Security xyz2064',
+      type: 'Observability',
+      provider: 'GCP - Iowa (us-central1)',
+    },
+  ];
+
+  return (
+    <CloudHomepageLayout>
+      <EuiSpacer size="m" />
+      <EuiPanel>
+        <EuiFlexGroup gutterSize="s" alignItems="center">
+          <EuiFlexItem grow={false}>
+            <div css={logoCircle}>
+              <EuiIcon type="logoElasticsearch" size="m" />
+            </div>
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiFlexGroup alignItems="center" justifyContent="spaceBetween">
+              <EuiFlexItem>
+                <EuiText>
+                  <h4>Elasticsearch deployments</h4>
+                </EuiText>
+              </EuiFlexItem>
+              <EuiFlexItem grow={false}>
+                <EuiLink href="">View all deployments</EuiLink>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+        <EuiSpacer sizer="1" />
+        <EuiTable css={tableStyles}>
+          <EuiTableHeader key="header">
+            {TABLE_HEADER_DEPLOYMENTS.map(header => (
+              <EuiTableHeaderCell key={header.name}>
+                {header}
+              </EuiTableHeaderCell>
+            ))}
+          </EuiTableHeader>
+          <EuiTableBody>
+            <EuiTableRow key="row">
+              {TABLE_ITEMS_DEPLOYMENTS.map(item => (
+                <Fragment key={item.name}>
+                  <EuiTableRowCell>
+                    <EuiLink onClick={() => router.push('/search')}>
+                      {item.name}
+                    </EuiLink>
+                  </EuiTableRowCell>
+                  <EuiTableRowCell>{item.status}</EuiTableRowCell>
+                  <EuiTableRowCell>{item.version}</EuiTableRowCell>
+                  <EuiTableRowCell>{item.region}</EuiTableRowCell>
+                  <EuiTableRowCell>
+                    <EuiLink href="">Manage</EuiLink>
+                  </EuiTableRowCell>
+                </Fragment>
+              ))}
+            </EuiTableRow>
+          </EuiTableBody>
+        </EuiTable>
+        <EuiSpacer size="s" />
+      </EuiPanel>
+      <EuiSpacer size="l" />
+      <EuiPanel>
+        <EuiFlexGroup gutterSize="s" alignItems="center">
+          <EuiFlexItem grow={false}>
+            <div css={logoCircle}>
+              <EuiIcon type="logoElasticsearch" size="m" />
+            </div>
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiFlexGroup alignItems="center" justifyContent="spaceBetween">
+              <EuiFlexItem>
+                <EuiText>
+                  <h4>Elasticsearch projects</h4>
+                </EuiText>
+              </EuiFlexItem>
+              <EuiFlexItem grow={false}>
+                <EuiLink href="">View all projects</EuiLink>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+        <EuiSpacer sizer="1" />
+        <EuiTable css={tableStyles}>
+          <EuiTableHeader key="header">
+            {TABLE_HEADER_PROJECTS.map(header => (
+              <EuiTableHeaderCell key={header.name}>
+                {header}
+              </EuiTableHeaderCell>
+            ))}
+          </EuiTableHeader>
+          <EuiTableBody>
+            <EuiTableRow key="row">
+              {TABLE_ITEMS_PROJECTS.map(item => (
+                <Fragment key={item.name}>
+                  <EuiTableRowCell>
+                    <EuiLink onClick={() => router.push('/search')}>
+                      {item.name}
+                    </EuiLink>
+                  </EuiTableRowCell>
+                  <EuiTableRowCell>{item.type}</EuiTableRowCell>
+                  <EuiTableRowCell>{item.provider}</EuiTableRowCell>
+                  <EuiTableRowCell>
+                    <EuiLink href="">Manage</EuiLink>
+                  </EuiTableRowCell>
+                </Fragment>
+              ))}
+            </EuiTableRow>
+          </EuiTableBody>
+        </EuiTable>
+        <EuiSpacer size="s" />
+      </EuiPanel>
+    </CloudHomepageLayout>
+  );
+};
+
+export default CloudHomepage;
